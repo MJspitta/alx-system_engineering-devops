de23d583e092186b1b0b9e4965329ca3823252cb
Madu Jang <madu.jang93@gmail.com>
2023-09-26 02:59:27 -0700

    client configuration file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,18 +2,14 @@
 # without typing a password
 include stdlib
 
-file { 'NO password':
-    ensure             => present,
-    path               => '/etc/ssh/ssh_config',
-    line               => '    PasswordAuthentication no',
-    match              => '^PasswordAuthentication',
-    append_on_no_match => true,
-}
-
-file { 'New private key holder':
-    ensure             => present,
-    path               => '/etc/ssh/ssh_config',
-    line               => '    IdentityFile ~/.ssh/school',
-    match              => '^IdentityFile',
-    append_on_no_match => true,
+file_line {
+  'Turn off passwd auth':
+    ensure => present,
+    path => '/etc/ssh/ssh_config',
+    line => '    PasswordAuthentication no'
+  ;
+  'Declare identity file':
+    ensure => present,
+    path => '/etc/ssh/ssh_config',
+    line => '    IdentityFile ~/.ssh/school'
 }
