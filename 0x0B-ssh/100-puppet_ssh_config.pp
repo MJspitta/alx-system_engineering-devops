78f916d4a05734485817f51bff11914eeda2eacf
Madu Jang <madu.jang93@gmail.com>
2023-11-29 03:27:54 -0800

    client config file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# using Puppet to make changes to our configuration file
+
+file { 'etc/ssh/ssh_config':
+	ensure => present,
+	content => "
+			#SSH CLIENT CONFIGURATION
+			host*
+			IdentityFile ~/.ssh/school
+			PasswordAuthentication no
+}
