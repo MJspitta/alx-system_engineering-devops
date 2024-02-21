414f34b54b604d5d72db49fb3bffd86d03c36d7c
Madu Jang <madu.jang93@gmail.com>
2024-02-21 14:34:49 -0800

    fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,30 +4,26 @@ Using a REST API, for a given employee ID,
 returns information about his/her TODO list progress
 """
 import requests
-from sys import argv
-
-
-def fetch_todo():
-    """ get data from api and display todo list progress """
-    url = "http://jsonplaceholder.typicode.com/"
-    user_id = argv[1]
-    user_resp = requests.get(url + "users/{}".format(user_id))
-    user = user_resp.json()
-
-    params = {"userId": user_id}
-    todos_resp = requests.get(url + "todos", params=params)
-    todos = todos_resp.json()
-    complete_list = []
-
-    for td in todos:
-        if td.get("completed") is True:
-            complete_list.append(td.get("title"))
-    print ("Employee {} is done with tasks({}/{})".format(user.get("name"),
-                                                          len(complete_list),
-                                                          len(todos)))
-    for c in complete_list:
-        print("\t{}".format(c))
+import sys
 
 
 if __name__ == "__main__":
-    fetch_todo()
+    """ get data from api and display todo list progress """
+    url = "http://jsonplaceholder.typicode.com/"
+    employee_id = sys.argv[1]
+    user_response = requests.get(url + "users/{}".format(employee_id))
+    user = user_response.json()
+
+    params = {"userId": employee_id}
+    todos_response = requests.get(url + "todos", params=params)
+    todos = todos_response.json()
+    completed = []
+
+    for todo in todos:
+        if todo.get("completed") is True:
+            completed.append(todo.get("title"))
+    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
+                                                          len(completed),
+                                                          len(todos)))
+    for complete in completed:
+        print("\t {}".format(complete))
