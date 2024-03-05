61fd98d21f24acc42d49cf96fd281786681f1340
Madu Jang <madu.jang93@gmail.com>
2024-03-05 06:17:11 -0800

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,14 +10,13 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'My User Agent 1.0'}
     try:
-        resp = requests.get(url, headers=headers)
-        resp.raise_for_status()
-        data = resp.json()
-        subscribers = data.get('data', {}).get('subscribers', 0)
-        return subscribers
+        resp = requests.get(url, headers=headers, allow_redirects=False)
+        if resp.status_code == 200:
+            data = resp.json()
+            subscribers = data.get('data', {}).get('subscribers', 0)
+            return subscribers
+        else:
+            return 0
     except requests.RequestException as e:
         print("Error fetching data:", e)
-        return 0
-    except KeyError as e:
-        print("Error parsing JSON:", e)
         return 0
