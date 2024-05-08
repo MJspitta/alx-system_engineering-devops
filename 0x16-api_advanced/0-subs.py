31e1b844cbb67365ab015005cfdb658144c63cee
Madu Jang <madu.jang93@gmail.com>
2024-05-08 02:30:48 -0700

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,16 +8,10 @@ import requests
 def number_of_subscribers(subreddit):
     """returns number of subscribers for a given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
-    try:
-        resp = requests.get(url, headers=headers, allow_redirects=False)
-        if resp.status_code == 200:
-            data = resp.json()
-            subscribers = data.get('data', {}).get('subscribers')
-            return subscribers
-        else:
-            return 0
-    except requests.RequestException as e:
-        print("Error fetching data:", e)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    results = response.json().get('data')
+    return results.get('subscribers')
