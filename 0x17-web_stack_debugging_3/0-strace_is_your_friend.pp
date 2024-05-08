f2f92b04b4aee64dcbabbb5c0af559a1065c5af9
Madu Jang <madu.jang93@gmail.com>
2024-05-08 05:07:17 -0700

    strace

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# script to fix typo in config
+
+exec { 'fix config type':
+  command => "sed -i 's/.phpp/.php' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
